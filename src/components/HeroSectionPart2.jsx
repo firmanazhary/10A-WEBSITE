a2280dc231d7d25e8f89288662c1845d354797fc
barakaramadhan <barakaramadhan444@gmail.com>
2025-11-18 13:50:37 +0700

    tampilan

--- src/components/HeroSectionPart2.jsx
+++ src/components/HeroSectionPart2.jsx
@@ -3,11 +3,16 @@ import gambar from "../assets/gktau.webp"
 export default function HeroSectionPart2() {
     return( 
     <section className="bg-[linear-gradient(113.04deg,_#0A1A2F_13.32%,_#0F1F3D_67.63%)] flex flex-col">
-        <div className="w-full h-auto flex flex-col">
-            <h1 className="text-[60px] md:text-[220px] text-center font-bold font-proximaBlack text-white">ZENTHORIA</h1>
-            <h1 className="text-[60px] md:text-[220px] text-center font-bold font-proximaBlack text-white/20 -mt-32">ZENTHORIA</h1>
-            <h1 className="text-[60px] md:text-[220px] text-center font-bold text-transparent font-proximaBlack [-webkit-text-stroke:1px_white] -mt-32">ZENTHORIA</h1>
+        <div className="w-full h-auto flex flex-col lg:pl-5 md:pl-4 lg:pr-5  md:pr-4">
+            <h1 className="text-[60px] md:text-[134px] lg:text-[210px] text-center font-bold font-proximaBlack text-white hidden md:block"><span className="">ZEN</span>THORIA</h1>
+            <h1></h1>
+            <h1 className="text-[60px] md:text-[134px] lg:text-[210px] text-center font-bold font-proximaBlack text-white/20 lg:-mt-32 md:-mt-20 sm:mt-0">ZENTHORIA</h1>
+            <h1 className="text-[60px] md:text-[134px] lg:text-[210px] text-center font-bold text-transparent font-proximaBlack [-webkit-text-stroke:1px_white] lg:-mt-32 md:-mt-20">ZENTHORIA</h1>
         </div>
+
+        {/*  */}
+
+
 
     </section>)
 }
