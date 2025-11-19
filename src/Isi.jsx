1842e9c8df061d8c0d8eb6000e36b4234ea95b39
solobread12 <kahfi1211@student.abudzar.sch.id>
2025-11-19 10:06:21 +0700

    menambah file Presentasi

---
+++ src/Isi.jsx
@@ -0,0 +1,7 @@
+export default function Isi() {
+    return (
+        <div className="bg-[linear-gradient(to_bottom_right,_#234C6A,_#03233B)] min-h-screen">
+            
+        </div>
+    );
+}
