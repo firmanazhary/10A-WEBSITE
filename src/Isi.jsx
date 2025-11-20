e55a78a0e6d64ffadb0591790a3dc967a6831228
solobread12 <kahfi1211@student.abudzar.sch.id>
2025-11-20 12:49:06 +0700

    animasi

--- src/Isi.jsx
+++ src/Isi.jsx
@@ -2,7 +2,7 @@ import Presentasi from "./components/Presentasi.jsx";
 
 export default function Isi() {
     return (
-        <div className="bg-[linear-gradient(to_bottom_right,_#234C6A,_#03233B)] min-h-screen py-5 rounded-t-3xl">
+        <div className="bg-[linear-gradient(to_bottom_right,_#234C6A,_#03233B)] min-h-screen py-5 rounded-t-[100px] mt-20">
             <Presentasi />
         </div>
     );
