b10db9e90971f4478493b4492dd7118023f38d53
solobread12 <kahfi1211@student.abudzar.sch.id>
2025-11-28 10:36:57 +0700

    menambah card momen

--- src/Isi.jsx
+++ src/Isi.jsx
@@ -1,12 +1,9 @@
 import MomenPenting from "./components/MomenPenting.jsx";
-import HomeroomTeacher from "./components/HomeroomTeacher.jsx";
-import Gallery from "./components/Gallery.jsx";
 
 export default function Isi() {
     return (
         <div className="bg-[linear-gradient(to_bottom_right,_#234C6A,_#03233B)] min-h-screen py-5 rounded-t-[40px] lg:rounded-t-[100px] mt-20">
             <MomenPenting />
-            <Gallery />
         </div>
     );
 }
