52ce479e8f5426bb7e4644cdcc04eed21ddfc05f
barakaramadhan <barakaramadhan444@gmail.com>
2025-12-01 09:49:33 +0700

    update

--- src/components/Navigasi.jsx
+++ src/components/Navigasi.jsx
@@ -4,9 +4,36 @@ import { section } from "framer-motion/m";
 
 export default function Navigasi(){
 
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 40) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return(
         <section>
-          
+            <div 
+              className={`fixed right-[32px] top-20 py-[12px] px-[30px] transition-colors duration-300 rounded-full
+                ${scrolled 
+                  ? "bg-blue-400/40 backdrop-blur-md  shadow-lg border border-white z-30"
+                  : "bg-transparent"
+                }`}
+            >
+              <ul className="flex gap-[50px]">
+                <li><a href="" className="font-vietnam font-bold text-[18px] text-white">HOME</a></li>
+                <li><a href="" className="font-vietnam font-bold text-[18px] text-white">OUR PROFILE</a></li>
+                <li><a href="" className="font-vietnam font-bold text-[18px] text-white">OUR GALLERY</a></li>
+              </ul>
+            </div>
         </section>
     )
 }
