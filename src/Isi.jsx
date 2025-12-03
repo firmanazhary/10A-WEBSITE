b11a3ca098669cb9fdd08c3608be4d0822fa10bc
solobread12 <kahfi1211@student.abudzar.sch.id>
2025-12-03 11:56:30 +0700

    menambah section homeroom

--- src/Isi.jsx
+++ src/Isi.jsx
@@ -1,9 +1,11 @@
 import MomenPenting from "./components/MomenPenting.jsx";
+import HomeroomTeacher from "./components/HomeroomTeacher.jsx";
 
 export default function Isi() {
     return (
         <div className="">
             <MomenPenting />
+            <HomeroomTeacher />
         </div>
     );
 }
--- src/components/HomeroomTeacher.jsx
+++ src/components/HomeroomTeacher.jsx
@@ -1,31 +1,61 @@
+import bgSection from '../assets/story/bghomeroom.webp';
+import ustadz from "../assets/story/ustd.webp"
 
 
 export default function HomeroomTeacher() {
     return (
-        <section className="w-[100%] bg-white rounded-[40px] flex flex-col items-center relative z-10 h-auto px-2">
-            <div className="w-full">
-                <img src="" alt="" className="rounded-[40px] w-full h-auto"/>
-                <h1 className="bg-[#FF9D00] font-proximaBlack lg:text-[30px] px-2 text-white absolute top-0 lg:right-10 md:text-[20px] md:right-10">Homeroom Teacher</h1>
-            </div>
-            <div>
-                <div className="flex flex-col relative gap-5">
-                    <div className="text-left">
-                        <h1 className="lg:text-[80px] text-[24px] text-[#F87B1B] font-bold font-proximaBlack">Muhammad</h1>
-                        <h1 className="lg:text-[245px] text-[75px] text-[#3E4B63] font-bold font-proximaBlack lg:-mt-[100px] -mt-[25px]">Firman</h1>
-                    </div>
-                    <div className="text-center lg:-mt-[180px] -mt-[60px]">
-                        <h1 className="lg:text-[245px] text-[75px] text-[#03233B]/50 font-bold font-proximaBlack">Azhary</h1>
-                    </div>
-                </div>
-                <div className="flex flex-row relative gap-5 w-full items-center -mt-[15px] lg:-mt-[50px]">
-                    <div className="">
-                        <img src=" " alt="" className="lg:w-[290px] h-auto w-[90px] "/>
-                    </div>
-                    <div>
-                        <p className="text-[#3E4B63] text-20px lg:text-[60px] font-proximaBlack">Belajar itu proses, bukan perlombaan</p>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <section className=''>
+<div style={{
+      backgroundImage: `url(${bgSection})`,
+      backgroundColor: "lightgray",
+      backgroundPosition: "-57.542px -377.992px",
+      backgroundSize: "146.635% 165.417%",
+      backgroundRepeat: "no-repeat",
+}} className='w-[94vw] h-[110vh] flex flex-row py-10 px-10 gap-10 relative rounded-lg mx-auto my-20'>
+     <div className="absolute inset-0 bg-[#0A66C2]/50 opacity-40 rounded-lg"></div>
+<div className='relative h-full w-1/2 flex flex-col justify-center items-start gap-5 px-6'>
+    <div className='h-1/3 w-full'>
+      <p className='font-birthstone font-bold text-white text-4xl'>pelita penerang</p>
+      <p className='font-birthstone font-bold text-white text-4xl'>
+        our <span className='font-vietnam'>HOMEROOM TEACHER</span>
+      </p>
+    </div>
+
+    <div className='h-1/3 w-full'>
+      <div className='w-80 h-64 bg-slate-400'></div>
+    </div>
+
+    <div className='h-1/3 w-full'>
+      <p className='font-vietnam text-white text-base'>Hello! I am a Software Engineering student at Politeknik IDN, where I am deeply passionate about learning new technologies and enhancing my programming skills. I also work as an IT teacher, guiding students in web development and other IT-related subjects. As a Web Developer, I create functional and visually appealing websites. Alongside these roles, I take on freelance projects to further expand my experience in the tech industry</p>
+    </div>
+  </div>
+
+  <div>
+    <img src="" alt="" />
+  </div>
+
+  <div>
+    <div>
+      <p>FIRMAN</p>
+    </div>
+
+    <div>
+      <p>AZHARY</p>
+    </div>
+
+    <div>
+      <div>
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
+      <button></button>
+    </div>
+  </div>
+</div>
+
+  
+</section>
+
     );
 }
